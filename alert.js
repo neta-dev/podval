40eed0d7497c07aa86ba2e0ad9444791a51acc6e
neta-dev <36772518+neta-dev@users.noreply.github.com>
2020-11-18 16:55:00 -0500

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,12 @@
+
+[1, 2].forEach(alert);
+
+// let name = "Koka"
+// alert(`Hello, ${name}`);
+// alert(`result: ${1 + 5}`)
+
+// let age = prompt('How old are you?', 51);
+// alert(`I'm ${age} year old`);
+
+let name = prompt("What is your name?");
+alert(`My name is ${name}`);
